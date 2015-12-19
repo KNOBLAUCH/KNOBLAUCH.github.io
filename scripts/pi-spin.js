8d347af28b7c0fe9dbfc936ef005a4a0b8d0d8d7
KNOBLAUCH <gijsdewortel@gmail.com>
2015-12-19 14:18:00 +0100

    Update pi-spin.js

--- scripts/pi-spin.js
+++ scripts/pi-spin.js
@@ -14,7 +14,7 @@ spinCount.textContent = 'π is al 0 keer gespind';
     { 
       count++;
       piCount.textContent = pi.slice(0,count+2);
-      spinCount.textContent = 'π is al ' + count + ' keer gespind';
+      spinCount.textContent = 'π is al ' + 2 + ' keer gespind';
     }
     loop()
   }, 1000);
