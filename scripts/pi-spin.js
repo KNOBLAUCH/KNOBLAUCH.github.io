a72a2374027486582a74b19a6bda8aaa9583d489
KNOBLAUCH <gijsdewortel@gmail.com>
2015-12-19 13:19:50 +0100

    Update pi-spin.js

--- scripts/pi-spin.js
+++ scripts/pi-spin.js
@@ -2,7 +2,7 @@ var myHeading = document.querySelector('p2');
 myHeading.textContent = '3.141';
 
 function doSetTimeout(i) {
-  setTimeout(function() { myHeading.textContent += i; }, 1000);
+  setTimeout(function() { myHeading.textContent += i; }, 1000000);
 }
 
 for (var i = 0; i <= 10; ++i)
