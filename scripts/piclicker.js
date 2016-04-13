a14eb985c64abb6524510f8af4b0e2ce6758a2c3
KNOBLAUCH <gijsdewortel@gmail.com>
2016-04-13 22:30:21 +0200

    Update piclicker.js

--- scripts/piclicker.js
+++ scripts/piclicker.js
@@ -1,9 +1,22 @@
+var amountofpis = document.querySelector('p2');
+amountofpis.textContent = 'amount of πs:';
+
+var pispersec = document.querySelector('p3');
+pispersec.textContent = 'amount of πs per second:';
+
 var pis = 0;
+var pps = 0.7;
 
 (function loop() {
   setTimeout(function () {
     { 
       //update every second
+      
+      amountofpis.textContent = 'amount of πs: ' + pis;
+      pis += pps;
+      
+      pispersec.textContent = 'amount of πs per second: ' + pps;
+      
     }
     loop()
   }, 1000);
