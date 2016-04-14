1a4679cd80b5b6a087badfab99dcd19c77385762
KNOBLAUCH <gijsdewortel@gmail.com>
2016-04-14 09:03:07 +0200

    Update piclicker.js

--- scripts/piclicker.js
+++ scripts/piclicker.js
@@ -14,7 +14,7 @@ var totpis = 0;
     { 
       //update every 1/100 second
       
-      pisRound = Math.round(pis*100)/100;
+      pisRound = Math.round(pis*10)/10;
       ppsRound = Math.round(pps*1000)/1000;
       
       amountofpis.textContent = 'amount of πs: ' + pisRound;
